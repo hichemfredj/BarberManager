f65df7f929434a680f46797312fa5f2a0b8a06a9
Hichem <hichemfredj123@gmail.com>
2020-09-18 09:20:26 -0400

    no message

---
+++ back-end/src/test/java/cal/barbermanager/BarbermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cal.barbermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbermanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
