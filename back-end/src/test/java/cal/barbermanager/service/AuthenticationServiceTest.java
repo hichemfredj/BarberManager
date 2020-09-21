cfcc65d12a5aad8e20010a6aa92b9062bb74f7c8
Hichem <hichemfredj123@gmail.com>
2020-09-21 12:25:13 -0400

    tester le AuthenticationService quand le token est invalid, methode : authenticate_invalidRequest().

--- back-end/src/test/java/cal/barbermanager/service/AuthenticationServiceTest.java
+++ back-end/src/test/java/cal/barbermanager/service/AuthenticationServiceTest.java
@@ -58,9 +58,39 @@ public class AuthenticationServiceTest {
         assertEquals(user.getUniqueId(),jwtLoginDTO.getUserUniqueId());
         assertNotNull(jwtLoginDTO.getToken());
 
-
-
-
-
     }
+
+    @Test
+    public void authenticate_invalidRequest() {
+
+        // Arrange
+
+        AuthenticationService service = new AuthenticationService(userRepository, jwtProvider, passwordEncoder);
+
+        LoginDTO loginDTO = new LoginDTO();
+
+        loginDTO.setEmail("test@test.com");
+        loginDTO.setPassword("123456789");
+
+        User user = new User();
+
+        user.setUniqueId(UUID.randomUUID());
+        user.setEmail(loginDTO.getEmail());
+        user.setPasswordHash(passwordEncoder.encode(
+                "This is a different password then the request"));
+
+        Mockito.when(userRepository.findByEmail(Mockito.any())).thenReturn(Optional.of(user));
+
+        // Act & Assert
+
+        JwtLoginDTO jwtLoginDTO = service.authenticate(loginDTO);
+
+        assertNotNull(jwtLoginDTO);
+
+        assertNull(jwtLoginDTO.getUserUniqueId());
+        assertNull(jwtLoginDTO.getToken());
+    }
+
+
+
 }
